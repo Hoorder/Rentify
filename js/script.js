cc0722060f094fe2361e62b53db01cbe925ab7a7
Kamil Czenczek <praca.hoorder@o2.pl>
2024-07-05 23:54:49 +0200

    Finish admin site

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const editButtons = document.querySelectorAll("#editButton");
+const editExitButton = document.querySelector("#exitBtn");
+const editContainer = document.querySelector(".edit__container");
+
+editButtons.forEach(editButton => (editButton.addEventListener('click', ()=> {
+    editContainer.classList.add("edit__container--visible");
+})))
+
+editExitButton.addEventListener('click', ()=> {
+    editContainer.classList.remove("edit__container--visible");
+})
